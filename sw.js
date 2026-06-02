0924ac1846a83338aab46c610e2906514b297448
bmalapitvany-ops <bmalapitvany@gmail.com>
2026-06-02 22:37:05 +0200

    add sw

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "bevetel-v2";
+const FILES = ["/bevetel-app/", "/bevetel-app/index.html", "/bevetel-app/manifest.json"];
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
